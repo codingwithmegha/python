b0b26a20d2fb2972b6f18f29f286b1841b3453ea
Megha Tomar <122961155+codingwithmegha@users.noreply.github.com>
2025-03-22 10:38:28 +0530

    Add files via upload

---
+++ printstatement.py
@@ -0,0 +1,9 @@
+#WAP to display a person's name, age and address in three different lines.
+
+
+name=input("enter the name")
+print(name)
+age=int(input("enter the age"))
+print(age)
+address=input("enter the address")
+print(address)
