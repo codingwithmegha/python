d78020a776e5eb40fdc3625464fb6d814cf0bb6b
Megha Tomar <122961155+codingwithmegha@users.noreply.github.com>
2025-03-22 10:38:45 +0530

    Add files via upload

---
+++ student_details.py
@@ -0,0 +1,13 @@
+#WAP to take details from a student for ID-card and then print it in different lines.
+
+
+college=input("enter your college name  ")
+name=input("enter your name  ")
+phone=int(input("enter phone number  "))
+address=input("enter your address  ")
+stream=input("enter stream  ")
+print(college)
+print(name)
+print(phone)
+print(address)
+print(stream)
