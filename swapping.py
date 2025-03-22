76a9201ae44b57375dac443283557d710e232a86
Megha Tomar <122961155+codingwithmegha@users.noreply.github.com>
2025-03-22 10:39:01 +0530

    Add files via upload

---
+++ swapping.py
@@ -0,0 +1,13 @@
+#WAP to swap two variables.
+
+
+num1=input("enter the num1 :")
+print(num1)
+num2=input("enter the num2 :")
+print(num2)
+temp=num1
+num1=num2
+num2=temp
+print("after swapping")
+print(num1)
+print(num2)
