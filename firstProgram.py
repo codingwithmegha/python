6a858d3d3a367b3f209d3deaebcc9f10d17de329
Megha Tomar <122961155+codingwithmegha@users.noreply.github.com>
2025-03-22 10:37:55 +0530

    Add files via upload

---
+++ firstProgram.py
@@ -0,0 +1,42 @@
+print("Hello world")
+print("megha tomar")
+print("Shivek Dhama")
+print("megha tomar\n"
+      "shivek dhama\n"
+      "its mohan nagar ghaziabad")
+print("""hello world
+this is my first python program""")
+"""Hiiiiiii
+iama  w jkdn kjncdskjsam xs k
+kmdeckemd"""
+
+#
+# a='hello'
+# print(a)
+#
+# #user input
+# name=input("enter ur name:")
+# print(name)
+#
+# #integer input
+# age=int(input("enter age"))
+# print(age)
+
+#evalute
+# exp=eval(input("enter something"))
+# print(exp)
+
+#type casting
+name="megha"
+print(type(name))
+
+age=44
+print(type(age))
+
+a="123"
+b=1.24
+a=float(a)
+print(type(a))
+c=a+b
+print(c)
+print(type(c))
