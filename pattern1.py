1903300f6c70c0ddf70ad6d6e5cde46598153a98
Megha Tomar <122961155+codingwithmegha@users.noreply.github.com>
2025-03-28 16:42:53 +0530

    Add files via upload

---
+++ pattern1.py
@@ -0,0 +1,15 @@
+for i in range(1,6):
+    for j in range(1,i+1):
+        print(j,end=" ")
+    print()
+
+
+for i in range(1,11):
+    if i==3:
+        print("add this song to the favs")
+    else:
+        print(i)
+
+for i in range(1,101):
+    if i%8==0 and i%12==0:
+        print(i)
