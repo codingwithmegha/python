b299587085d008f8a28ebcf8e57af9b212327bd9
Megha Tomar <122961155+codingwithmegha@users.noreply.github.com>
2025-03-29 23:34:45 +0530

    Add files via upload

---
+++ question.py
@@ -0,0 +1,28 @@
+# A="OOTD.YOLO.ASAP.BRB.GTG.OTW"
+"""WAP to separate the following string
+into comoo(,) separated values."""
+
+a="OOTD.YOLO.ASAP.BRB.GTG.OTW"
+b=a.split(".")
+print(b)
+
+#WAP to sort strings alphabetically in python.
+c="hello"
+d=sorted(c)
+print(d)
+
+#WAP to remove the character from the string
+a="hello"
+b=a.replace("e"," ")
+print(b)
+
+#Z="F.R.I.E.N.D.S."
+# WAP to remove dot(.) from the following string
+Z="F.R.I.E.N.D.S."
+print(Z.replace(".",""))
+
+#WAP to check the number of occurance of a substring in a string
+a="she seels seashells on the sea shore"
+b=a.count("sea")
+print(b)
+
