b551df1cf6f4df4137af4a4e7b8fe6183fc1c72c
Megha Tomar <122961155+codingwithmegha@users.noreply.github.com>
2025-03-29 23:35:17 +0530

    Add files via upload

---
+++ String_Slicing.py
@@ -0,0 +1,11 @@
+#String Slicing
+a="Harry Potter and the Goblet of fire"
+b="0123456789"
+print(a)
+print(b[::2])
+print(b[:7:2])
+print(a[0:5])
+print(a[6:12])
+print(a[:5])
+print(a[-4:])
+print(b[6::-1])
